da549ade32eda597d75d5aba7d6761bd8ccf69fc
JRodrigoNogueira <j.rodrigo.lima.2017@gmail.com>
2022-05-15 00:45:42 -0300

    Version 1.6

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let desenhos = []
 let verifLetra = []
 let acertos
 let errosMax = 7
+let pontuacao = 0
 let erros
 let acertou
 let jogando
@@ -24,7 +25,9 @@ let letraDica
 let msgDica
 let letraDigitada
 let temas
+let msgDisplay
 let msg
+let solicitaDica = 0
 
 window.addEventListener("load", inicializar)
 
@@ -39,7 +42,7 @@ function inicializar() {
     desenhos[6] = document.getElementById("pernaD")
 
     jog = document.getElementById("letraX")
-    msg = document.getElementById("msg")
+    msgDisplay = document.getElementById("msg")
     cabeca = document.getElementById("cabeca")
     letrasDigitDisplay = document.getElementById("letrasDigitadas")
     msgDica = document.getElementById("msgDica")
@@ -47,15 +50,14 @@ function inicializar() {
     temas = document.getElementById("temas")
     btnModal = document.getElementsByClassName("btnModal")
 
-    jog.focus()
-
     inicio()
 }
 
 //Preparo para um novo jogo
 function inicio() {
     removerModal()
-    msg.innerHTML = ""
+    solicitaDica = 0
+    msgDisplay.innerHTML = ""
     cabeca.src = "img/cabeca1.png"
     jogando = true
     cont = 0
@@ -67,7 +69,6 @@ function inicio() {
     palavraInit = palavra
     tam = palavra.length
     jog.value = ""
-    jog.focus()
     letrasDigitDisplay.innerHTML = "Letras Digitadas:"
     for (let i = 0; i < tam; i++) {
         document.getElementById("letra" + i).style.background = "white"
@@ -96,28 +97,27 @@ function defineLetras(l) {
 function jogar() {
     if (jogando) {
         laco = true
-        jog.focus()
         letraDigitada = jog.value.toUpperCase()
         lerLetra(letraDigitada)
         //Testando se a letra já foi digitiada
         for (let i = 0; i < 30; i++) {
             if (verifLetra[i] == letraDigitada) {
                 laco = false
-                msg.innerHTML = "Você já tentou essa letra!"
+                msgDisplay.innerHTML = "Você já tentou essa letra!"
             }
         }
         //Testando se é uma letra válida
-        if (!letraValida) {
-            msg.innerHTML = "Digite apenas letras!"
-        }//Testando se algo foi digitado
-        else if (letraDigitada == "") {
-            msg.innerHTML = "Digite uma letra!"
+        if (letraDigitada == "") {
+            msgDisplay.innerHTML = "Digite uma letra!"
+        } //Testando se algo foi digitado
+        else if (!letraValida) {
+            msgDisplay.innerHTML = "Digite apenas letras!"
         } else if (laco) {
             verifLetra[cont] = letraDigitada
             let letra
             let pesq
             let letraTmp
-            msg.innerHTML = ""
+            msgDisplay.innerHTML = ""
             cont++
             letra = letraDigitada
             jog.value = ""
@@ -145,6 +145,8 @@ function jogar() {
                     cabeca.src = "img/cabeca2.png"
                     letrasDigitDisplay.innerHTML += ` ${letra.toUpperCase()}.`
                     msg = "Sinto muito, você perdeu!"
+                    msgDica.style.color = "white"
+                    pontuacao = 0
                     for (let i = 0; i < tam; i++) {
                         if (document.getElementById("letra" + i).value == "") {
                             document.getElementById("letra" + i).style.background = "#f91880"
@@ -157,7 +159,41 @@ function jogar() {
                 }
                 //Teste de vitória
             } else if (acertos == tam) {
-                msg = "Parabéns, você ganhou!"
+                pontuacao += 1
+                if(pontuacao < 10){
+                    msg = `Parabéns, você ganhou!<br>Sua pontuação atual é de: ${pontuacao}`
+                }else {
+                    msgDica.style.color = "#00ba7c"
+                    switch(temas.value){
+                        case "t1":
+                            msg = `Você acertou ${pontuacao} palavras seguidas!<br>Você é um mestre no modo padrão!!!`
+                            break
+                        
+                        case "t2":
+                            msg = msg = `Você acertou ${pontuacao} palavras seguidas!<br>Você é um desafiante no LoL!!!`
+                            break
+
+                        case "t3":
+                            msg = msg = `Você acertou ${pontuacao} palavras seguidas!<br>Você é um mestre dos filmes!!!`
+                            break
+
+                        case "t4":
+                            msg = msg = `Você acertou ${pontuacao} palavras seguidas!<br>Você é um mestre das séries!!!`
+                            break
+
+                        case "t5":
+                            msg = msg = `Você acertou ${pontuacao} palavras seguidas!<br>Você é um mestre dos jogos!!!`
+                            break
+
+                        case "t6":
+                            msg = msg = `Você acertou ${pontuacao} palavras seguidas!<br>Você é um mestre dos animes!!!`
+                            break
+
+                        case "t7":
+                            msg = msg = `Você acertou ${pontuacao} palavras seguidas!<br>Você é um mestre do Kpop!!!`
+                            break
+                    }
+                }
                 jogando = false
                 iniciarModal(msg,jogando)
             }
@@ -176,7 +212,7 @@ function recebeEnter(event) {
 
 //Chamada da Função Dica
 function dica() {
-    if (jogando) {
+    if ((jogando)&&(solicitaDica<2)) {
         let verifica = false
         let sort = Math.floor(Math.random() * tam)
         while (!verifica) {
@@ -185,11 +221,15 @@ function dica() {
             } else {
                 letraDica = palavraInit.charAt(sort)
                 verifica = true
+                solicitaDica += 1
                 msg = `Dica: Letra ${letraDica}.`
                 iniciarModal(msg,jogando)
             }
         }
-        jog.focus()
+    } else if ((jogando)&&(solicitaDica>=2)){
+        solicitaDica += 1
+        msg = `Você já usou todas as dicas desta rodada!`
+        iniciarModal(msg,jogando)
     }
 }
 
@@ -248,7 +288,30 @@ function trocaTema() {
                 "Warcraft", "Metalgear", "Portal", "Starcraft", "Pong", "Inside", "Valorant", "Simcity", "OverWatch", "Bloodborne"]
             qt = biblioteca.length - 1
             break
+
+        case "t6":
+            biblioteca = ["Rezero", "Naruto", "Danganronpa", "Pokemon", "Digimon", "Noragami", "Another", "Fate", "Castlevania", "Hellsing",
+                "Kakeguri", "Dororo", "Boruto", "Berserker", "Bleach", "Haikyuu", "Beyblade", "Orange", "Beatless", "Ultraman", 
+                "Aggretsuko", "Durarara", "Shiki", "Shirobako", "Magi", "Sasuke", "Pain", "Kakashi", "Madara", "Hashirama", 
+                "Goku", "Mikasa", "Eren", "Takemichi", "Hinata", "Naoto", "Kira", "L", "Luffy", "Ash", 
+                "Akame", "Yumeko", "Monokuma", "Chihiro", "Emilia", "Pikachu", "Dracula", "Alucard", "Sarada", "Guts", 
+                "Shoyo", "Kenma", "Shinoa", "Guren", "Meliodas", "Izumi", "Kirito", "Ken", "Kaneki", "Touka"]
+            qt = biblioteca.length -1
+            break
+
+        case "t7":
+            biblioteca = ["SKZ", "TXT", "BTS", "ATEEZ", "Jungkook", "Hyunjin", "Felix", "Taehyung", "SHINee", "LeeKnow", 
+                "Wooyoung", "Enhypen", "Yeonjun", "Beomgyu", "PSY", "ITZY", "BLACKPINK", "Lisa", "Suga", "AESPA", 
+                "Karina", "CL", "BIGBANG", "SUPERJUNIOR", "TWICE", "Taehyun", "SOOBIN", "Bangchan", "Changbin", "Jimin",
+                "Rose", "Jisoo", "Hyuna", "Dawn", "EXO", "Mamamoo", "Nmixx", "MCND", "NCT", "Seventeen",
+                "TVXQ", "Namjoon", "HueningKai", "IKON", "Ryunjin", "Chaeryeong", "Lia", "MonstaX", "Seungmin", "JHope",
+                "LeSserafim", "RedVelvet", "Bahiyyih", "Winter", "Jessi", "Minho", "Jonghyun", "Key", "Onew", "Taemin", 
+                "Kai", "Sunoo", "Jake", "Jay", "Jungwon", "Treasure", "IVE", "Jin"]
+            qt = biblioteca.length - 1
+            break
     }
+    pontuacao = 0
+    msgDica.style.color = "white"
     inicio()
 }
 
@@ -256,7 +319,10 @@ function trocaTema() {
 function iniciarModal(msg,jogo) {
     msgDica.innerHTML = msg
     modal.classList.add("mostrar")
-    if(jogo){
+    if((jogo)&&(solicitaDica>=3)){
+        btnModal[0].classList.remove("ocultar")
+        btnModal[1].classList.add("ocultar")
+    }else if(jogo){
         btnModal[0].classList.add("ocultar")
         btnModal[1].classList.remove("ocultar")
     }else if(!jogo){
